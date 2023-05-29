d853df610451f6d5bfcb5474edfc858dfaf99e6d
Murf-y <charbelfayad64@gmail.com>
2023-05-29 21:08:43 +0300

    feat: add graph datastrucutre :fire:

--- graph.js
+++ graph.js
@@ -1,10 +1,28 @@
-// graph implementation using adjacency matrix
+import { ColorEnum } from "./main.js";
+
+class Node {
+  constructor(element, color, index) {
+    this.element = element;
+    this.color = color;
+    this.index = index;
+  }
+
+  changeColor(color) {
+    this.color = color;
+    this.element.style.backgroundColor = color;
+  }
+}
 
 class Graph {
-  constructor(nodes) {
-    //   nodes is an array of objects
-    this.nodes = nodes;
-    this.numberOfVertices = nodes.length;
+  constructor(circles) {
+    //   nodes is an array of circles (html div elements)
+    //  trasnform it into Node objects
+    this.nodes = [];
+    for (let i = 0; i < circles.length; i++) {
+      this.nodes[i] = new Node(circles[i], ColorEnum.NONE, i);
+    }
+
+    this.numberOfVertices = this.nodes.length;
     this.matrix = [];
     for (let i = 0; i < this.numberOfVertices; i++) {
       this.matrix[i] = [];
@@ -31,7 +49,7 @@ class Graph {
   }
 
   getNeighbors(vertex) {
-    //   Returns an array of neighbors (objects not indices)
+    //  Returns an array of Node objects
 
     let neighbors = [];
     for (let i = 0; i < this.numberOfVertices; i++) {
@@ -41,6 +59,47 @@ class Graph {
     }
     return neighbors;
   }
+
+  getNode(i) {
+    return this.nodes[i];
+  }
+
+  isValidColor(vertex, color) {
+    let neighbors = this.getNeighbors(vertex);
+    for (let i = 0; i < neighbors.length; i++) {
+      if (neighbors[i].color === color) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  getValidColors(vertex) {
+    let validColors = [];
+    for (let color in ColorEnum) {
+      if (this.isValidColor(vertex, ColorEnum[color])) {
+        validColors.push(ColorEnum[color]);
+      }
+    }
+    return validColors;
+  }
+
+  isSatisfied() {
+    for (let i = 0; i < this.numberOfVertices; i++) {
+      if (!this.isValidColor(i, this.nodes[i].color)) {
+        return false;
+      }
+    }
+
+    // make sure all nodes have a color
+    for (let i = 0; i < this.numberOfVertices; i++) {
+      if (this.nodes[i].color === ColorEnum.NONE) {
+        return false;
+      }
+    }
+
+    return true;
+  }
 }
 
-export default Graph;
+export { Graph, Node };
